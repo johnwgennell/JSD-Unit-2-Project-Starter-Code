1509e3bd7ffe2cc7cb0f23c00c2bdc900ae1e7c5
John <john@johnwgennell.com>
2017-07-06 19:39:06 -0400

    first draft of feedr code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
-/*
-  Please add all Javascript code to this file.
-*/
+$(document).ready(function(){
+
+  $( "#popUp" ).removeClass('hidden');
+  $( "#popUp" ).click(function() {
+    $( "#popUp" ).addClass('hidden');
+  });
+
+    var request = $.ajax({
+        url:"https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json",
+        data: status
+    });
+
+    request.done(function(feed){
+        console.log(feed);
+        var rssDigg = status;
+        console.log(status);
+    });
+
+    var request = $.ajax({
+        url:"https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json",
+    });
+
+  request.done(function(data){
+  			console.log(data)
+  });
+
+  request.done(function(feed) {
+      console.log(feed);
+  });
+
+  var request = $.ajax({
+      url:"https://www.reddit.com/top.json",
+  });
+
+  request.done(function(feed){
+      console.log(feed);
+  });
+
+
+
+});
