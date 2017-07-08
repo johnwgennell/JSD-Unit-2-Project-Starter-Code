ae464b51f5e7189b6b4127fbfd03920fa52d7f16
John <john@johnwgennell.com>
2017-07-08 18:26:51 -0400

    app.js edits

--- js/app.js
+++ js/app.js
@@ -1,26 +1,41 @@
+/* The Feedr app pulls in JSON and displays articles */
+
+'use strict';
+
 $(document).ready(function(){
 
+// Show loader while app is loading and hide it when user selects the "X" button
   $( "#popUp" ).removeClass('hidden');
   $( "#popUp" ).click(function() {
     $( "#popUp" ).addClass('hidden');
   });
 
+// Show the options from the body list in the dropdown search
+  $('body').on('click', '#NewsSources li', function(){
+	var dropdownText = $(this).text();
+	$('#sourceName').text(dropdownText);
+	$(this).siblings().removeClass('selected');
+	$(this).addClass('selected');
+
+// Digg RSS feed
+    var diggFeed = {};
+
     var request = $.ajax({
         url:"https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json",
-        success: {status, feed}
+        data: {status, feed}
     });
+    
+      request.done(function(feed){
+          console.log(feed);
+          var rssDigg = status;
+          alert(status);
+          for(var i=0;i<feed.length;i++){
+    	 			console.log(feed[i]);
+    	 			var feedObjects = {
 
-    request.done(function(feed){
-        console.log(feed);
-        var rssDigg = status;
-        alert(status);
-        for(var i=0;i<feed.length;i++){
-  	 			console.log(feed[i]);
-  	 			var feedObjects = {
-
-          }
-    });
-
+            }
+          });
+        });
 
 //
 //
@@ -30,26 +45,28 @@ $(document).ready(function(){
 //
 //
 //
+
+// Mashable RSS feed
+    var mashableFeed = {};
 
     var request = $.ajax({
         url:"https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json",
     });
 
-  request.done(function(data){
-  			console.log(data)
-  });
-
-  request.done(function(feed) {
+    request.done(function(feed) {
       console.log(feed);
   });
 
-  var request = $.ajax({
-      url:"https://www.reddit.com/top.json",
-  });
+// Reddit RSS feed
+    var redditFeed = {};
 
-  request.done(function(feed){
-      console.log(feed);
-  });
+    var request = $.ajax({
+        url:"https://www.reddit.com/top.json",
+    });
+
+    request.done(function(feed){
+        console.log(feed);
+    });
 
 
 
